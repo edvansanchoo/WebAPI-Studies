03fb87a69db72704f4b9821214488984237621e6
edvansanchoo <edvansanchoo@gmail.com>
2023-09-02 18:14:33 -0400

    Create? controller and Model

---
+++ WebAPI-Studies/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI_Studies.Models;
+
+namespace WebAPI_Studies.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<EmployeeModel>> GetAllEmployees() 
+        {
+            return Ok(new List<EmployeeModel>() {new EmployeeModel("jonas", 15, "local/teste.txt"), new EmployeeModel("Marcos", 25, "local/teste1.txt") });
+        }
+    }
+}
---
+++ WebAPI-Studies/Models/EmployeeModel.cs
@@ -0,0 +1,17 @@
+﻿namespace WebAPI_Studies.Models
+{
+    public class EmployeeModel
+    {
+        public int id { get; private set; }
+        public string name{ get; private set; }
+        public int age { get; private set; }
+        public string? photo { get; private set; }
+
+        public EmployeeModel(string name, int age, string photo)
+        {
+            this.name = name;
+            this.age = age;
+            this.photo = photo;
+        }
+    }
+}
