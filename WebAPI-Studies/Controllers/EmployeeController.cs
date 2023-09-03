e03b8ce044c1052145564a54feeb0fce74277fec
edvansanchoo <edvansanchoo@gmail.com>
2023-09-02 21:02:57 -0400

    created endpoint to save and get photos type .png

--- WebAPI-Studies/Controllers/EmployeeController.cs
+++ WebAPI-Studies/Controllers/EmployeeController.cs
@@ -14,13 +14,18 @@ namespace WebAPI_Studies.Controllers
 
         public EmployeeController(IEmployeeRepository employeeRepository)
         {
-            _employeeRepository = employeeRepository ?? throw new ArgumentNullException();
+            _employeeRepository = employeeRepository ?? throw new Exception("Fail Injection Employee Repository");
         }
 
         [HttpPost]
-        public IActionResult Add(EmployeeViewModel employeeViewModel)
+        public IActionResult Add([FromForm] EmployeeViewModel employeeViewModel)
         {
-            var employee = new EmployeeModel(employeeViewModel.Name, employeeViewModel.Age, null);
+            var filePath = Path.Combine("Storage", employeeViewModel.Photo.FileName);
+
+            using Stream fileStream = new FileStream(filePath, FileMode.Create);
+            employeeViewModel.Photo.CopyTo(fileStream);
+
+            var employee = new EmployeeModel(employeeViewModel.Name, employeeViewModel.Age, filePath);
             _employeeRepository.Add(employee);
             return Ok();
         }
@@ -29,6 +34,27 @@ namespace WebAPI_Studies.Controllers
         public IActionResult GetAll()
         {
             return Ok(_employeeRepository.GetAll());
+        }
+
+        [HttpPost]
+        [Route("{id}/download")]
+        public IActionResult DownloadPhoto(int id) 
+        {
+            var employee = _employeeRepository.GetById(id);
+            if(employee == null)
+            {
+                return this.NotFound($" Could not find User: {id}");
+            }
+
+            try
+            {
+                var dataBytes = System.IO.File.ReadAllBytes(employee.photo);
+                return File(dataBytes, "image/png");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
     }
 }
